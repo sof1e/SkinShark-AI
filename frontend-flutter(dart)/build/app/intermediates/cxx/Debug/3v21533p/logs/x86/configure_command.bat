0ccce5887add1c127602a158be1a4413e41a3a01
sof1e <sofiia.honcharova21@gmail.com>
2025-04-25 23:31:45 +0300

    Added back and model folders

---
+++ frontend-flutter(dart)/build/app/intermediates/cxx/Debug/3v21533p/logs/x86/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"C:\\Users\\thron\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\flutter\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\groovy" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\thron\\AppData\\Local\\Android\\sdk\\ndk\\26.3.11579264" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\thron\\AppData\\Local\\Android\\sdk\\ndk\\26.3.11579264" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\thron\\AppData\\Local\\Android\\sdk\\ndk\\26.3.11579264\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\thron\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\flutterapps\\skinshark-ai\\build\\app\\intermediates\\cxx\\Debug\\3v21533p\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\flutterapps\\skinshark-ai\\build\\app\\intermediates\\cxx\\Debug\\3v21533p\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\flutterapps\\skinshark-ai\\android\\app\\.cxx\\Debug\\3v21533p\\x86" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
